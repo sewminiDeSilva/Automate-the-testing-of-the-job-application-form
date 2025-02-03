4d4f92ba0870404c2a1118cc8b545b5cdc1dd8bc
sewminiDeSilva <imesha20005@gmail.com>
2025-02-03 12:45:43 +0530

    edit CI/CD pipeline for playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  timeout: 120_000, // Increase global timeout to 120 seconds
+  
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
